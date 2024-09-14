09c851f5d43c7fe3937653fb1fb89109eef00c91
heedrhiss <muyideenidris77@gmail.com>
2024-09-14 18:27:07 +0100

    appLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import GeneralStyle from "./styles/GeneralStyle"
-import Button from './ui/Button';
-import Input from './ui/Input';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
     <>
-    <GeneralStyle/>
-    <h1>Hello world</h1>
-    <Button>Check In</Button>
-    <Button>Check Out</Button>
-    <Input type="text" placeholder="Enter name"/>
-    <Input type="number" placeholder="Enter number"/>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
