28ede90c2364d1646e1212d3b2a93f4150aa4bf9
heedrhiss <muyideenidris77@gmail.com>
2024-09-16 12:58:49 +0100

    addSupabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://wwaxeaycsqvbzzmodmpx.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3YXhlYXljc3F2Ynp6bW9kbXB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzMzczMTEsImV4cCI6MjA0MTkxMzMxMX0.-fThd4U6PiUbB6qBrpxUxCwlh6utbZcGitxF73NeTZA"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
