e60baf1c3b4cc8da7382800de2cacc4bbbc50741
heedrhiss <muyideenidris77@gmail.com>
2024-09-17 15:58:14 +0100

    reactQuery

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+React router DOM
+styled-components
+supabase
+React Query (@tanstack/react-query)
+React Query devtools (@tanstack/react-query-devtool)
+Date function library (date-fns)
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,14 +1,19 @@
-// import { useEffect } from "react";
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from '../services/cabinsAPI'
+import { useQuery } from "@tanstack/react-query";
+// import { useEffect } from "react";
 
 
 function Cabins() {
-  useEffect(function(){
-    getCabins().then(data=> console.log(data))
-  },[])
+  // useEffect(function(){
+  //   getCabins().then(data=> console.log(data))},[])
+
+    const {isLoading, data: cabins, error} =  useQuery({
+      queryKey: ['cabin'],
+      queryFn: getCabins
+      })
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
